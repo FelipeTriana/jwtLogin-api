52fb5db64c62fce0fd955ad42934feb735ad8150
FelipeTriana <felipe.trianag@udea.edu.co>
2020-12-14 08:23:14 -0500

    Cors configuration

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const admin = require('./token-validation/admin');
 require('dotenv').config();
 
 const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.use(bodyparser.urlencoded({
     extended: false
